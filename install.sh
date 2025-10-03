75b16a32dd5ccdff3578745b0b428cebfc67e368
jacen-terra <jacen@terra.tld>
2025-10-03 15:14:54 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-pip install --refresh "common/[tests]"
+pip install "common/[tests]"
 pip install "core/[all,tests]" "features/" "tabular/[all,tests]" "multimodal/[tests]" "timeseries/[all,tests]" "eda/" "autogluon/"
